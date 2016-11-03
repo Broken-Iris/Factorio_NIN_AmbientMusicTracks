2eb21c585834d02a7bad0066869331fd30bc5aa1
Broken-Iris <rtsguy002@yahoo.com>
2016-11-03 04:53:23 -0600

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,616 @@
+data:extend(
+{
+{
+    type = "ambient-sound",
+    name = "1",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/1.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "2",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/2.ogg"
+    }
+  },
+  {
+    type = "ambient-sound",
+    name = "3",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/3.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "4",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/4.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "5",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/5.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "6",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/6.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "7",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/7.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "8",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/8.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "9",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/9.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "10",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/10.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "11",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/11.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "12",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/12.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "13",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/13.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "14",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/14.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "15",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/15.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "16",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/16.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "17",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/17.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "18",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/18.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "19",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/19.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "20",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/20.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "21",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/21.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "22",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/22.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "23",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/23.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "24",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/24.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "25",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/25.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "26",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/26.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "27",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/27.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "28",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/28.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "29",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/29.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "30",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/30.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "31",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/31.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "32",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/32.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "33",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/33.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "34",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/34.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "35",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/35.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "36",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/36.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "37",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/37.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "38",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/38.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "39",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/39.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "40",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/40.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "41",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/41.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "42",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/42.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "43",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/43.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "44",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/44.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "45",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/45.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "46",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/46.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "47",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/47.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "48",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/48.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "49",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/49.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "50",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/50.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "51",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/51.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "52",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/52.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "53",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/53.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "54",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/54.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "55",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/55.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "56",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/56.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "57",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/57.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "58",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/58.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "59",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/59.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "60",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/60.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "61",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/61.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "62",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/62.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "63",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/63.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "64",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/64.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "65",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/65.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "66",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/66.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "68",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/68.ogg"
+    }
+  },
+    {
+    type = "ambient-sound",
+    name = "69",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__NIN_AmbientMusicTracks__/69.ogg"
+    }
+  }
+}
+)
